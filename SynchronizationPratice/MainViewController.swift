a229ad06ba88aaf7ffdd634b32e705c85f4b210b
Donghee Lee <ehdgml9419@gmail.com>
2022-04-18 21:43:24 +0900

    Main Thread UI 변경

--- SynchronizationPratice/MainViewController.swift
+++ SynchronizationPratice/MainViewController.swift
@@ -20,60 +20,122 @@ class MainViewController: UIViewController {
     let loginButton = UIButton()
     let idValidView = UIView()
     let pwValidView = UIView()
+    let textView = UITextView()
+    
+    let jsonlink = "http://headers.jsontest.com"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        onLoad(jsonlink)
         bind(viewModel)
         attribute()
         layout()
     }
     
+    func onLoad(_ url: String) {
+        let observable = downloadJson(URLs: jsonlink)
+        observable.subscribe { event in
+            switch event {
+            case let .next(json):
+                DispatchQueue.main.async {
+                    self.textView.text = json
+                }
+            case .completed:
+                break
+            case .error:
+                break
+            }
+        }
+        .disposed(by: disposeBag)
+    }
+    
     func bind(_ viewModel: MainViewModel) {
         
-        let idinputOb: Observable<String> = idField.rx.text.orEmpty.asObservable()
-        let idValidOb: Observable<Bool> = idinputOb.map(checkEmailValid(_:))
-        
-        let pwInputOb: Observable<String> = pwField.rx.text.orEmpty.asObservable()
-        let pwValidOb: Observable<Bool> = pwInputOb.map(checkPasswordValid(_:))
-        
-        idValidOb
-            .subscribe(onNext: { b in
-                self.idValidView.isHidden = b
-            })
-            .disposed(by: disposeBag)
-        
-        pwValidOb
-            .subscribe(onNext: { b in
-                self.pwValidView.isHidden = b
-            })
-            .disposed(by: disposeBag)
-        
+        //Input
         idField.rx.text.orEmpty
-            .map(checkEmailValid)
-            .subscribe(onNext: { b in
-                self.idValidView.isHidden = b
-            })
+            .bind(to: viewModel.idInputText)
+            .disposed(by: disposeBag)
+        
+        viewModel.idInputText
+            .map(viewModel.checkEmailValid(_:))
+            .bind(to: viewModel.idValid)
             .disposed(by: disposeBag)
         
         pwField.rx.text.orEmpty
-            .map(checkPasswordValid)
+            .bind(to: viewModel.pwInputText)
+            .disposed(by: disposeBag)
+        
+        viewModel.pwInputText
+            .map(viewModel.checkPasswordValid(_:))
+            .bind(to: viewModel.pwValid)
+            .disposed(by: disposeBag)
+
+
+
+        
+        loginButton.rx.tap
+            .withLatestFrom(viewModel.idInputText)
+            .bind(to: viewModel.jsonSub)
+            .disposed(by: disposeBag)
+        
+        //Output
+        viewModel.idValid
             .subscribe(onNext: { b in
-                self.pwValidView.isHidden = b
-                self.loginButton.isEnabled = b
+                self.idValidView.isHidden = b
             })
             .disposed(by: disposeBag)
         
+        viewModel.pwValid
+            .subscribe(onNext: { b in
+                self.pwValidView.isHidden = b
+            })
+            .disposed(by: disposeBag)
+
         Observable
             .combineLatest(
-                idValidOb,
-                pwValidOb,
+                viewModel.idValid,
+                viewModel.pwValid,
                 resultSelector: { $0 && $1 }
             )
             .subscribe(onNext: { b in
-                self.loginButton.isEnabled = b
+                switch b {
+                case true:
+                    self.loginButton.isEnabled = b
+                    self.loginButton.setTitle("로그인", for: .normal)
+                case false:
+                    self.loginButton.isEnabled = b
+                    self.loginButton.setTitle("입력정보 확인", for: .normal)
+                }
             })
             .disposed(by: disposeBag)
+        
+        viewModel.jsonSub
+            .subscribe(onNext: { text in
+                self.textView.text = text
+            })
+            .disposed(by: disposeBag)
+        
+    }
+    
+    func downloadJson(URLs url: String) -> Observable<String?> {
+        //1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
+        return Observable.create { emitter in
+            let url = URL(string: url)!
+            let task = URLSession.shared.dataTask(with: url) { data, _, err in
+                guard err == nil else {
+                    emitter.onError(err!)
+                    return
+                }
+                if let dat = data, let json = String(data: dat, encoding: .utf8) {
+                    emitter.onNext(json)
+                }
+                emitter.onCompleted()
+            }
+            task.resume()
+            return Disposables.create {
+                task.cancel()
+            }
+        }
     }
     
     private func attribute() {
@@ -89,14 +151,16 @@ class MainViewController: UIViewController {
         loginButton.isEnabled = false //초기값
         loginButton.tintColor = .systemGray
         loginButton.backgroundColor = .systemGray
-        loginButton.setTitle("비활성화 상태", for: .normal)
-        loginButton.addTarget(self, action: #selector(showNextView), for: .touchDown)
+        loginButton.setTitle("입력정보 확인", for: .normal)
+//        loginButton.addTarget(self, action: #selector(showNextView), for: .touchDown)
         
         idValidView.backgroundColor = .red
         idValidView.layer.cornerRadius = 10
         
         pwValidView.backgroundColor = .red
         pwValidView.layer.cornerRadius = 10
+        
+        textView.backgroundColor = .white
     }
     
     @objc func showNextView() {
@@ -105,7 +169,7 @@ class MainViewController: UIViewController {
     
     private func layout() {
     
-        [idField, pwField, loginButton, idValidView, pwValidView]
+        [idField, pwField, loginButton, idValidView, pwValidView, textView]
             .forEach {
                 view.addSubview($0)
             }
@@ -138,13 +202,23 @@ class MainViewController: UIViewController {
             $0.trailing.equalTo(pwField.snp.trailing).inset(10)
             $0.top.equalTo(pwField.snp.top).inset(20)
         }
+        
+        textView.snp.makeConstraints {
+            $0.top.equalTo(loginButton.snp.bottom).offset(25)
+            $0.leading.trailing.equalTo(idField)
+            $0.height.equalTo(250)
+        }
+        
     }
     
-    private func checkEmailValid(_ email: String) -> Bool {
-        return email.contains("@") && email.contains(".")
+    func downloadJson(_ url: String) -> String {
+        //let url = URL(string: <#T##String#>)
+        return ""
+    }
+    
+    func onLoad() {
+        textView.text = ""
+        
     }
 
-    private func checkPasswordValid(_ password: String) -> Bool {
-        return password.count > 5
-    }
 }
--- SynchronizationPratice/MainViewModel.swift
+++ SynchronizationPratice/MainViewModel.swift
@@ -13,5 +13,23 @@ import SnapKit
 struct MainViewModel {
     let disposeBag = DisposeBag()
     
+    let idValid = BehaviorSubject<Bool>(value: false)
+    let idInputText = BehaviorSubject<String>(value: "")
     
+    let pwValid = BehaviorSubject<Bool>(value: false)
+    let pwInputText = BehaviorSubject<String>(value: "")
+    
+    let jsonValid = BehaviorSubject<Bool>(value: false)
+    let jsonSub = BehaviorSubject<String>(value: "")
+
+    //view에서 행해지는 로직을 viewModel로 옮기고 결과가 같은지 확인할것(4/17)
+    func checkEmailValid(_ email: String) -> Bool {
+        return email.contains("@") && email.contains(".")
+    }
+
+    func checkPasswordValid(_ password: String) -> Bool {
+        return password.count > 5
+    }
+    
+
 }
